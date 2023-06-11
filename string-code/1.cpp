559c30cc220213277c08d1cf0e0e97ba1951f1c4
binod.yadav <binod.yadav@nslhub.com>
2023-06-11 13:30:49 +0530

    celery pod changes

--- string-code/1.cpp
+++ string-code/1.cpp
@@ -1,1 +1,13 @@
-dkoifdlmds;lm
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"enter a no";
+    int a;
+    cin>>a;
+    cout<<a;
+    cout<<endl;
+
+    cout<<"output\n";
+    
+}
